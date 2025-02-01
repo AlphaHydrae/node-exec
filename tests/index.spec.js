6afb6338e1585c8f5683f8837caf7552703e7340
Simon Oulevay <simon.oulevay@gmail.com>
2025-02-01 21:45:21 +0100

    Fix tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,6 +2,6 @@ const { execSync } = require('node:child_process')
 
 describe('exec', () => {
   test('it should work', () => {
-    expect(execSync('node index2.js').toString()).toBe('Hello\n')
+    expect(execSync('node index2.js').toString()).toBe('index.spec.js\n')
   })
 })
