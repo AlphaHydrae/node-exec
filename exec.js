7b3dbf4233c447815530cbfc4640bbf3b05b799f
Simon Oulevay <simon.oulevay@gmail.com>
2025-02-05 00:00:29 +0100

    Allow customizing the first argument to execvp

--- exec.js
+++ exec.js
@@ -4,7 +4,7 @@ const native = require('./index.js');
  * Performs the execvp system call with the given file and arguments, replacing
  * the current process image with the new one.
  *
- * This function does not return if successful, as the current process is
+ * This function does not return if successful, as the current process image is
  * replaced by the new one. An error may be thrown, with an error message
  * containing the error code returned by execvp.
  *
@@ -15,12 +15,23 @@ const native = require('./index.js');
  * @param {string} file The file to execute. If not a path, the PATH environment
  *                      variable is searched.
  * @param {string[]} args The arguments to pass to the new process. The `file`
- *                        argument is automatically prepended to the arguments.
+ *                        argument is automatically prepended to the arguments
+ *                        and passed as the first argument to the `execvp`
+ *                        system call.
+ * @param {Object} [options] Optional options.
+ * @param {string} [options.arg0=file] The value to pass as the first argument
+ *                                     to the `execvp` system call. Defaults to
+ *                                     the `file` argument.
  * @throws {Error} If the execvp system call fails.
  */
-module.exports.execvp = function (file, args) {
+module.exports.execvp = function (file, args, options = {}) {
+  const arg0 = options.arg0 ?? file;
+
+  // Prevent the standard streams from being closed when the process is
+  // replaced.
   native.doNotCloseOnExit(process.stdin.fd);
   native.doNotCloseOnExit(process.stdout.fd);
   native.doNotCloseOnExit(process.stderr.fd);
-  native.execvp(file, [file, ...args]);
+
+  native.execvp(file, [arg0, ...args]);
 };
